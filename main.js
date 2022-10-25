6f73eeaaf60a7d34d808a6026b6089f0c34d8b68
Marcos Hernan Parella <marcosparella2000@gmail.com>
2022-10-24 23:00:05 -0300

    Desafío 1 terminado

--- main.js
+++ main.js
@@ -1,55 +1,59 @@
-class Persona {
-    #nombre
-    #colores
-
-    constructor( nombre ) {
-        this.#nombre = nombre
-        this.#colores = []
-    }
-
-    getName() {
-        return this.#nombre
-    }
-
-    getColors() {
-        return this.#colores
-    }
-
-    addColor() {
-        this.#colores.push( color )
-    }
-}
-
 class Container {
     #id
     #save
 
     constructor( object ) {
         this.#id = object
-        this.#save = []
+        this.#save = [ this.#id ]
     }
 
     save( object ) {
         this.#save.push( object )
     }
 
-    getById( id ) { 
-        if ( id === null ) {
-            return null 
+    getById( object ) { 
+        if ( object.id === null ) {
+            return null
         } else {
             return this.#id 
         } 
     }
 
     getAll() {
-        return this.#save
+        const object = [this.#save]
+        return  object
     }
 
     deleteById( id ) {
         return this.#save.splice( id )
     }
 
-    deteleAll( id ) {
-        return this.#save.splice( 0 , id.lenght)
+    deteleAll() {
+        let getId = this.#id;;
+        return getId = [];
     }
 }
+
+const car = {
+    id: 1,
+    title: 'Peugeout 308 GTI 2012',
+    price: 3000000,
+    thumbnail: "https://i.pinimg.com/564x/cd/61/22/cd6122d08693a7a0cc12059078bf3081.jpg"
+}
+
+const products = new Container( car )
+
+products.save()
+console.log(products)
+
+const trygetById = products.getById( car )
+console.log( trygetById )
+
+const trygetAll = products.getAll()
+console.log( trygetAll )
+
+const tryDeleteById = products.deleteById( 1 )
+console.log( tryDeleteById )
+
+const tryDeleteAll = products.deteleAll()
+console.log( tryDeleteAll )
